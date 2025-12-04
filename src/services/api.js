4b5a3c8d5a68f626740be232587076e7db1910cf
Adarsh-shet <adarshshet1998@gmail.com>
2025-12-04 23:50:13 +0530

    Update api.js
    xma

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,25 @@
 const BASE_URL = process.env.REACT_APP_API_URL;
 
-// AGE API
+// AGE
 export const calculateAge = async (dob) => {
     const res = await fetch(`${BASE_URL}/api/calc/age?dob=${dob}`);
-    return res.text();
+    return res.json();
 };
 
-// EXPERIENCE API
+// EXPERIENCE
 export const calculateExperience = async (startDate, endDate) => {
     const res = await fetch(`${BASE_URL}/api/calc/experience?startDate=${startDate}&endDate=${endDate}`);
-    return res.text();
+    return res.json();
 };
 
-// GENERAL CALCULATOR
+// GENERAL
 export const calculateGeneral = async (num1, num2, operation) => {
     const res = await fetch(`${BASE_URL}/api/calc/${operation}?a=${num1}&b=${num2}`);
+    return res.json();
+};
+
+// EMI
+export const calculateEmi = async (p, r, m) => {
+    const res = await fetch(`${BASE_URL}/api/calc/emi?principal=${p}&rate=${r}&months=${m}`);
     return res.json();
 };
