5e75ac5f53994e0ac988aa4d5e2a4c882906cf0d
Adarsh-shet <adarshshet1998@gmail.com>
2025-12-04 23:04:07 +0530

    Update api.js
    cksm

--- src/services/api.js
+++ src/services/api.js
@@ -3,20 +3,17 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 // AGE API
 export const calculateAge = async (dob) => {
     const res = await fetch(`${BASE_URL}/api/calc/age?dob=${dob}`);
-    if (!res.ok) throw new Error("API Error");
-    return res.json();
+    return res.text();
 };
 
 // EXPERIENCE API
-export const calculateExperience = async (startDate) => {
-    const res = await fetch(`${BASE_URL}/api/calc/experience?startDate=${startDate}`);
-    if (!res.ok) throw new Error("API Error");
-    return res.json();
+export const calculateExperience = async (startDate, endDate) => {
+    const res = await fetch(`${BASE_URL}/api/calc/experience?startDate=${startDate}&endDate=${endDate}`);
+    return res.text();
 };
 
 // GENERAL CALCULATOR
 export const calculateGeneral = async (num1, num2, operation) => {
     const res = await fetch(`${BASE_URL}/api/calc/${operation}?a=${num1}&b=${num2}`);
-    if (!res.ok) throw new Error("API Error");
     return res.json();
 };
