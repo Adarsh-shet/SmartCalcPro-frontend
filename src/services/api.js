6283adeae5e64f9cf3fe86051fe0be9c3dae6cd2
Adarsh-shet <adarshshet1998@gmail.com>
2025-12-04 20:11:37 +0530

    Update api.js
    ys

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,19 @@
-const BASE_URL = process.env.REACT_APP_API_URL;  // No trailing slash!
+const BASE_URL = "https://smartcalcpro-backend.onrender.com/api/calc"; // No trailing slash!
 
 // AGE API
 export const calculateAge = async (dob) => {
-    const res = await fetch(`${BASE_URL}/api/calc/age?dob=${dob}`);
+    const res = await fetch(`${BASE_URL}/age?dob=${dob}`);
     return res.json();
 };
 
 // EXPERIENCE API
 export const calculateExperience = async (startDate) => {
-    const res = await fetch(`${BASE_URL}/api/calc/experience?startDate=${startDate}`);
+    const res = await fetch(`${BASE_URL}/experience?startDate=${startDate}`);
     return res.json();
 };
 
 // GENERAL CALCULATOR
 export const calculateGeneral = async (num1, num2, operation) => {
-    const res = await fetch(`${BASE_URL}/api/calc/${operation}?a=${num1}&b=${num2}`);
+    const res = await fetch(`${BASE_URL}/${operation}?a=${num1}&b=${num2}`);
     return res.json();
 };
